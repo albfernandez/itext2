11b691b1853b82d2300e20d00dcb78602fb30424
albfernandez <infjaf@gmail.com>
2016-03-11 21:54:31 +0100

    Tests

---
+++ src/test/java/com/lowagie/text/pdf/AcroFieldsTest.java
@@ -0,0 +1,30 @@
+package com.lowagie.text.pdf;
+
+import java.io.ByteArrayOutputStream;
+
+import org.junit.Test;
+
+import com.lowagie.text.RunAllExamplesTest;
+
+public class AcroFieldsTest {
+    
+    @Test
+    public void testSetFields() throws Exception {
+        singleTest(RunAllExamplesTest.RESOURCES_DIR +"register.xfdf");
+    }
+
+    @Test
+    public void testListInSetFields() throws Exception {
+        singleTest(RunAllExamplesTest.RESOURCES_DIR +"list_register.xfdf");
+    }
+    
+    private void singleTest(String xfdfFileName) throws Exception {
+        // merging the FDF file
+        PdfReader pdfreader = new PdfReader(RunAllExamplesTest.RESOURCES_DIR +"SimpleRegistrationForm.pdf");
+        PdfStamper stamp = new PdfStamper(pdfreader, new ByteArrayOutputStream());
+        XfdfReader fdfreader = new XfdfReader(xfdfFileName);
+        AcroFields form = stamp.getAcroFields();
+        form.setFields(fdfreader);
+        stamp.close();
+    }
+}
