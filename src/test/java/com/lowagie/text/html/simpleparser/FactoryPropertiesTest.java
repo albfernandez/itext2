65553bc5731789fb73455740bc2f44d2f95d53b0
Alberto Fernandez <infjaf@gmail.com>
2017-06-28 17:46:02 +0200

    Fix to support java 6

--- src/test/java/com/lowagie/text/html/simpleparser/FactoryPropertiesTest.java
+++ src/test/java/com/lowagie/text/html/simpleparser/FactoryPropertiesTest.java
@@ -10,7 +10,7 @@ public class FactoryPropertiesTest {
 	 @Test
 	    public void shouldCreateRelativeLeadingForLineHeightNUmber() throws Exception {
 	        // given
-	        final HashMap h = new HashMap<>();
+	        final HashMap h = new HashMap();
 	        final String style = "line-height:1.4";
 	        h.put("style", style);
 	        final ChainedProperties cprops = new ChainedProperties();
