838bdf85f6d91c357a12c4b9a031a46c5f0e0845
Alberto Fernandez <infjaf@gmail.com>
2017-10-10 00:12:48 +0200

    Samples for text extraction

--- src/test/java/com/lowagie/text/pdf/parser/PdfTextExtractorTest.java
+++ src/test/java/com/lowagie/text/pdf/parser/PdfTextExtractorTest.java
@@ -4,13 +4,17 @@ package com.lowagie.text.pdf.parser;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.lowagie.text.Document;
+import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfReader;
+import com.lowagie.text.pdf.PdfWriter;
 
 
 public class PdfTextExtractorTest {
@@ -54,14 +58,50 @@ public class PdfTextExtractorTest {
 		Assert.assertTrue(result.contains("Remember to CATCH IT, BIN IT, KILL IT."));		
 	}
 	
+	@Test
+	public void testExtractFromEmptyPage() throws Exception {
+		String fileName = "example-empty-page.pdf";
+		String result1 = getString(fileName, 1);
+		Assert.assertEquals("Example 1 \n   ", result1);
+		String result2 = getString(fileName, 2);
+		Assert.assertEquals("   ", result2);
+		String result3 = getString(fileName, 3);
+		Assert.assertEquals("Example 3 ", result3);
+	}
+	
+	@Test
+	public void testExtractFromEmptyPage2() throws Exception {
+		Document document = new Document();
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PdfWriter.getInstance(document, baos);
+		document.open();
+		
+		document.add(new Paragraph("Example 1"));
+		document.newPage();
+		document.add(new Paragraph(" "));
+		document.newPage();
+		document.add(new Paragraph("Example 3"));
+		document.close();
+		
+		byte[] data = baos.toByteArray();
+		String result1 = getString(data, 1);
+		Assert.assertEquals("Example 1", result1);
+		String result2 = getString(data, 2);
+		Assert.assertEquals(" ", result2);
+		String result3 = getString(data, 3);
+		Assert.assertEquals("Example 3", result3);
+	}
+	
 	private String getString(String fileName, int pageNumber) throws Exception {
 		return getString(new File("src/test/resources", fileName), pageNumber);
 	}
 	private String getString(File file, int pageNumber) throws Exception {
-		byte[] pdfBytes = readDocument(file);
-		final PdfReader pdfReader = new PdfReader(pdfBytes);
-		
-		return new PdfTextExtractor(pdfReader).getTextFromPage( pageNumber);		
+		byte[] pdfBytes = readDocument(file);				
+		return getString(pdfBytes, pageNumber);
+	}
+	private String getString(byte[] pdfBytes, int pageNumber) throws Exception {
+		final PdfReader pdfReader = new PdfReader(pdfBytes);		
+		return new PdfTextExtractor(pdfReader).getTextFromPage( pageNumber);
 	}
 	
 	protected static byte[] readDocument(final File file) throws IOException {
