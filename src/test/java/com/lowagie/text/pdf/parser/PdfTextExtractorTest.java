64d72db9ed85b48f3df3166129a7c2dd6d99e310
Alberto Fernandez <infjaf@gmail.com>
2017-10-11 23:12:05 +0200

    Remove unused import

--- src/test/java/com/lowagie/text/pdf/parser/PdfTextExtractorTest.java
+++ src/test/java/com/lowagie/text/pdf/parser/PdfTextExtractorTest.java
@@ -4,7 +4,6 @@ package com.lowagie.text.pdf.parser;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
