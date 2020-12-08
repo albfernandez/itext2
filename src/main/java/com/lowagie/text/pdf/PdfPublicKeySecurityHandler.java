f8fad5cb298bb867e012c67feb1e27a6f482dffb
Alberto Fernandez <infjaf@gmail.com>
2020-12-08 21:18:42 +0100

    upgrade to bouncycastle 1.67
    Fixes #18

--- src/main/java/com/lowagie/text/pdf/PdfPublicKeySecurityHandler.java
+++ src/main/java/com/lowagie/text/pdf/PdfPublicKeySecurityHandler.java
@@ -92,7 +92,6 @@ package com.lowagie.text.pdf;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-
 import java.security.AlgorithmParameterGenerator;
 import java.security.AlgorithmParameters;
 import java.security.GeneralSecurityException;
@@ -100,18 +99,18 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
-
 import java.util.ArrayList;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
+import org.bouncycastle.asn1.ASN1Encoding;
 import org.bouncycastle.asn1.ASN1InputStream;
-import org.bouncycastle.asn1.ASN1Primitive;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.ASN1OutputStream;
+import org.bouncycastle.asn1.ASN1Primitive;
 import org.bouncycastle.asn1.DEROctetString;
-import org.bouncycastle.asn1.DEROutputStream;
 import org.bouncycastle.asn1.DERSet;
 import org.bouncycastle.asn1.cms.ContentInfo;
 import org.bouncycastle.asn1.cms.EncryptedContentInfo;
@@ -248,9 +247,9 @@ public class PdfPublicKeySecurityHandler {
             
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
             
-        DEROutputStream k = new DEROutputStream(baos);
+        final ASN1OutputStream derOutputStream = ASN1OutputStream.create(baos, ASN1Encoding.DER);
             
-        k.writeObject(obj);  
+        derOutputStream.writeObject(obj);  
         
         cms = baos.toByteArray();
 
